2111d57e1df234f8779456f164de9d28ce44a270
Casper V. Kristensen <casper@vkristensen.dk>
2026-01-17 21:34:55 +0100

    semi: false

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,12 @@
-import postgres from "postgres";
-import process from "node:process";
-import { Hono } from "hono";
+import postgres from "postgres"
+import process from "node:process"
+import { Hono } from "hono"
 
 const sql = postgres({
   host: "database",
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
-});
+})
 
 const begin_with_timeout = (body) => {
   return sql.begin(async (sql) => {
@@ -17,15 +17,15 @@ const begin_with_timeout = (body) => {
 
 await sql`
   create table if not exists "sheets" ( sheet jsonb );
-`;
+`
 await sql`
   create index idxsheets ON sheets using gin ( sheet );
-`;
+`
 
-const app = new Hono();
+const app = new Hono()
 
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
+  return c.text("Hello Hono!")
+})
 
-export default app;
+export default app
