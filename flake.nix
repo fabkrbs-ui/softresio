86c2b3dccc6e0b86b07b60cd4024c95edef93249
Asbjørn Kofoed-Nielsen <asbjorn@kofoednielsen.com>
2026-01-17 20:34:19 +0100

    Initial

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "softres.io";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      rec {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_24
+          ];
+        };
+      }
+    );
+}
