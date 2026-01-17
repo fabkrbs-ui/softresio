70a525b645350188058300e78b95b50031bd8c75
Casper V. Kristensen <casper@vkristensen.dk>
2026-01-17 20:56:06 +0100

    postgres

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,19 @@
+import postgres from "postgres";
+import process from "node:process";
+
+const sql = postgres({
+  host: "database",
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+});
+
+await sql`
+  create table if not exists "sheets" ( sheet jsonb );
+`;
+await sql`
+  create index idxsheets ON sheets using gin ( sheet );
+`;
+
 Deno.serve((_req) => {
   return new Response("Hello, World!");
 });
