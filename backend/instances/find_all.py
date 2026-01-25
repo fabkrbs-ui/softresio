e36b67b14d30f69b8db6e216ac3596f8527c605e
Asbjørn Kofoed-Nielsen <asbjorn@kofoednielsen.com>
2026-01-25 23:20:29 +0100

    Better item slots and types

--- backend/instances/find_all.py
+++ backend/instances/find_all.py
@@ -15,14 +15,47 @@ for item in items:
     slots.add(item["slot"])
 
 
+order = [
+    "Class",
+    "Token",
+    "Head",
+    "Neck",
+    "Shoulder",
+    "Back",
+    "Chest",
+    "Wrist",
+    "Hands",
+    "Waist",
+    "Legs",
+    "Feet",
+    "Finger",
+    "Trinket",
+    "Main Hand",
+    "Off Hand",
+    "Held In Off-hand",
+    "One-Hand",
+    "Two-Hand",
+    "Ranged",
+    "Relic",
+    "Quest Item",
+    "Mount",
+    "Bag",
+    "Profession",
+    "Materials",
+    "Consumable",
+    "Companion",
+    "Books",
+    "Miscellaneous"
+]
+
 print("{")
-for s in slots:
-    print(f'"{s}": [')
+for s in sorted(slots, key=lambda e: order.index(e)):
+    print(f'  "{s}": [')
     types = set()
     for item in items:
-        if item["slot"] == s:
+        if item["slot"] == s and item["type"] != None:
             types.add(item["type"])
     for typ in types:
-        print(f'  "{typ}",')
-    print("],")
+        print(f'    "{typ}",')
+    print("  ],")
 print("}")
